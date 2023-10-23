a3222d73d11a3aabb696a364afd2fb51dd3865bc
nataliasirt <nsirtakjob@gmail.com>
2023-10-22 23:17:22 -0500

    add test logout

---
+++ lesson1/test_logout.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+
+def test_logout_form():
+    driver.get("https://www.saucedemo.com/")
+    url_before = driver.current_url
+
+    username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
+    username_field.send_keys("standard_user")
+
+    password_field = driver.find_element(By.XPATH, '//input[@data-test="password"]')
+    password_field.send_keys("secret_sauce")
+
+    login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
+    login_button.click()
+
+    burger_menu = driver.find_element(By.ID, "react-burger-menu-btn")
+    burger_menu.click()
+
+
+    logout_button = driver.find_element(By.CSS_SELECTOR, "#logout_sidebar_link")
+    time.sleep(3)
+    logout_button.click()
+
+    url_after = driver.current_url
+    assert  url_after == url_before
+
+    driver.quit()
+
+
+
+
+
